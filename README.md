082ebb226c17fa1eb4ccdba2bf234ac9eeaa3399
rmbar <r.m.barnett@gmail.com>
2017-07-21 13:54:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,167 @@
+# What is AcceptPy?
+
+AcceptPy is a minimal acceptance test framework. It can be used to automatically and quickly check that a program operates as a developer intends.  What a unit test framework is for functions, AcceptPy is for whole programs.  
+
+For example we can use AcceptPy to test the operation of the console `echo` command. To do so we first author a test file (in JSON) which defines a test for the echo command:
+
+```
+{
+  "test_type": "shell command",
+  "command" : "echo have some pie.",
+  "expect_exit": 0,
+  "expect_stdout": "have some pie.\n"
+}
+```
+This test checks than executing `echo have some pie.` prints:
+
+```
+have some pie
+
+```
+
+to the console and then exits with code 0.
+
+We can run this test by invoking AcceptPy passing the directory that contains the test file:
+
+```
+python3 accept.py /home/user/echo_tests
+```
+
+which produces the following output:
+
+```
+#########################
+   Welcome to AcceptPy
+#########################
+
+searching for tests...
+
+/home/user/echo_test/echo_pie.test
+
+found 1 tests.
+
+
+#########################
+   Test 1 of 1
+#########################
+
+[running: /home/user/echo_tests/echo_pie.test]
+shell command: echo have some pie.
+<begin stdout>
+have some pie.
+
+<end stdout>
+[PASSED]
+
+
+#########################
+   Results:
+#########################
+
+
+1 of 1 tests passed.
+```
+
+When running the test defined in `echo_pie.test` AcceptPy:
+
+1. Launched a shell subprocess.
+2. Ran the command `echo have some pie.` in the shell.
+3. Checked that the command's exit code was 0.
+4. Checked that the total of the command's printed stdout was `have some pie.\n`
+
+# Installation
+
+# Running multiple tests
+
+Running multiple tests is accomplished by authoring multiple test files.
+
+For example:
+
+```
+$ echo '{ "test_type": "shell command", "command" : "echo dog" }' > echo_dog.test
+$ echo '{ "test_type": "shell command", "command" : "echo cat" }' > echo_cat.test
+$ python3 ~/accept.py ./
+```
+
+(n.b. in these minimal test files AcceptPy will only check that the echo commands terminated with exit code 0.)
+
+... prodcues the output:
+
+```
+#########################
+   Welcome to AcceptPy
+#########################
+
+searching for tests...
+
+./echo_cat.test
+./echo_dog.test
+
+found 2 tests.
+
+
+#########################
+   Test 1 of 2
+#########################
+
+[running: ./echo_cat.test]
+shell command: echo cat
+<begin stdout>
+cat
+
+<end stdout>
+[PASSED]
+
+
+#########################
+   Test 2 of 2
+#########################
+
+[running: ./echo_dog.test]
+shell command: echo dog
+<begin stdout>
+dog
+
+<end stdout>
+[PASSED]
+
+
+#########################
+   Results:
+#########################
+
+
+2 of 2 tests passed.
+```
+
+When running AcceptPy the program will reclusively search all directories under the given test directory argument
+for all files ending in `.test` or `.py`.
+
+# Types of tests
+
+## Shell command tests
+
+A shell command test is defined in a JSON file with the `.test` extension.  It is generally of the form:
+
+```
+{
+  "test_type": "shell command",
+  "command" : "echo have some pie.",
+  "expect_exit": 0,
+  "expect_stdout": "have some pie.\n"
+}
+```
+
+When a shell command test file is encountered AcceptPy will spawn a shell subprocess and execute the command string
+identified by `command`.  If `expect_exit` is specified AcceptPy will check that the command terminates with the given
+exit code.  If not, the tool will check that the command terminated with exit code 0.  If `expect_stdout` is specified
+AcceptPy will check that the text sent to standard out by the command matches the given text.
+
+## Python tests
+
+A Python test is defined in a `.py` file. When AcceptPy encounters such a file it will execute the Python program
+in a new subprocess using the command:
+
+`python3 file.py`
+
+and score the test as a pass if the returned exit code is 0. Any other exit code will result in a failed scoring.
---
+++ src/main/python/accept.py
@@ -0,0 +1,222 @@
+# Copyright (c) 2017 rmbar
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import argparse
+import json
+import os
+import subprocess
+
+#
+# Constant string prefixes and suffix for printing colors to the console.
+#
+HEADER = '\033[95m'
+OK_GREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+END_COLOR = '\033[0m'
+
+
+def run_test_from_python_test_file(test_file_path: str):
+    """Runs the given python file and returns True if the exit code is 0 and False otherwise.
+
+    test_file_path -- the path to the python file to run
+    """
+
+    print(HEADER + "[running: " + test_file_path + "]" + END_COLOR)
+
+    return run_shell_command("python3 " + test_file_path, 0)
+
+
+def run_test_from_json_test_file(test_file_path: str):
+    """Parses the given json test file and runs the defined test returning True if the tests passes and False otherwise.
+
+    Example json:
+
+    {
+      "test_type": "shell command",
+      "command" : "echo dog",
+      "expect_exit": 0,
+      "expect_stdout": "dog\n"
+    }
+
+    test_file_path -- the path to the json test file
+    """
+
+    print(HEADER + "[running: " + test_file_path + "]" + END_COLOR)
+
+    #
+    # Read and parse test_file_path as a JSON file.
+    #
+    with open(test_file_path, "r") as test_file:
+        test_file_content = test_file.read()
+
+    try:
+        test = json.loads(test_file_content)
+    except json.JSONDecodeError:
+        print(WARNING + "non-json test file: " + test_file_path + END_COLOR)
+        return False
+
+    #
+    # Determine the kind of test to run and run it.
+    #
+    test_type = test.get('test_type', None)
+    if test_type is None:
+        print(WARNING + "missing test_type in test file: " + test_file_path + END_COLOR)
+        return False
+
+    if test_type == "shell command":
+
+        command = test.get('command', None)
+
+        if command is None:
+            print(WARNING + "missing command in test file: " + test_file_path + END_COLOR)
+            return False
+
+        expect_exit = test.get('expect_exit', None)
+        expect_stdout = test.get('expect_stdout', None)
+
+        print(HEADER + "shell command: " + command + END_COLOR)
+        return run_shell_command(command, expect_exit, expect_stdout)
+    else:
+        print(WARNING + "unknown test type in test file: " + test_file_path + END_COLOR)
+        return False
+
+
+def run_shell_command(command: str, expected_exit: int, expected_stdout: str = None):
+    """Runs the given command string as a shell command in a new subprocess and returns whether the command
+       met the given expectations.
+
+    command -- the shell command to run e.g. "ls -l"
+    expected_exit -- the expected exit code of the shell command or None for no expectation
+    expected_stdout -- the expected standard out characters printed by the shell command or None for no expectation
+    """
+
+    #
+    # Run the given shell command and report standard out.
+    #
+    completed_process = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+
+    if len(completed_process.stdout) > 0:
+        print(HEADER + "<begin stdout>" + END_COLOR)
+        print(completed_process.stdout.decode('utf-8'))
+        print(HEADER + "<end stdout>" + END_COLOR)
+
+    #
+    # Check if the exit code and standard out match expectations if specified.
+    #
+    test_passed = True
+
+    if expected_stdout is not None and expected_stdout != completed_process.stdout.decode('utf-8'):
+        test_passed = False
+        print(FAIL + "<expected stdout>" + END_COLOR)
+        print(expected_stdout)
+        print(FAIL + "<expected stdout>" + END_COLOR)
+
+    if expected_exit is not None and expected_exit != completed_process.returncode:
+        test_passed = False
+        print(FAIL + "<expected error code " + str(expected_exit) + " but found " + str(completed_process.returncode) +
+              ">" + END_COLOR)
+
+    return test_passed
+
+
+if __name__ == "__main__":
+
+    #
+    # Determine the tests directory path from the command line arguments.
+    #
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tests_dir', type=str, help='the directory containing tests to run')
+    args = parser.parse_args()
+    tests_dir_path = args.tests_dir
+
+    #
+    # Find all test files.
+    #
+    test_file_paths = []
+    for root, dirs, files in os.walk(tests_dir_path):
+        for file in files:
+            if file.endswith(".test") or file.endswith(".py"):
+                test_file_paths.append(os.path.join(root, file))
+
+    #
+    # Report found test files to user
+    #
+    print(HEADER)
+    print("#########################")
+    print("   Welcome to AcceptPy   ")
+    print("#########################")
+    print(END_COLOR)
+
+    print("searching for tests...")
+    print("")
+    for file in test_file_paths:
+        print(file)
+
+    print("")
+    print("found " + str(len(test_file_paths)) + " tests.")
+    print("")
+
+    #
+    # Run each test file.
+    #
+    failed_test_files_paths = []
+
+    i = 1
+    for file in test_file_paths:
+        print(HEADER)
+        print("#########################")
+        print("   Test " + str(i) + " of " + str(len(test_file_paths)))
+        print("#########################\n" + END_COLOR)
+        if file.endswith(".test"):
+            passed = run_test_from_json_test_file(file)
+        else:
+            passed = run_test_from_python_test_file(file)
+
+        if passed:
+            print(OK_GREEN + "[PASSED]\n" + END_COLOR)
+        else:
+            failed_test_files_paths.append(file)
+            print(FAIL + "[FAILED]\n" + END_COLOR)
+
+        i = i + 1
+
+    print(HEADER)
+    print("#########################")
+    print("   Results:")
+    print("#########################\n" + END_COLOR)
+
+    #
+    # Report end of run statistics to user.
+    #
+    if len(failed_test_files_paths) > 0:
+        print("failed tests:")
+
+        for failed_test in failed_test_files_paths:
+            print(failed_test)
+
+    if len(failed_test_files_paths) > 0:
+        print(FAIL)
+    else:
+        print(OK_GREEN)
+
+    print(str(len(test_file_paths) - len(failed_test_files_paths)) + " of " + str(len(test_file_paths)) +
+          " tests passed.")
+    print(END_COLOR)
