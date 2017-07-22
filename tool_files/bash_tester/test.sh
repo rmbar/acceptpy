327611d03f4a74c414eaaf9a6839fe565ced52c9
rmbar <r.m.barnett@gmail.com>
2017-07-22 18:37:54 -0500

    add acceptance tests

---
+++ tool_files/bash_tester/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf out
+mkdir out
+
+python3 ../../src/main/python/accept.py tests/empty_file.test             > out/empty_file.txt
+python3 ../../src/main/python/accept.py tests/empty_json.test             > out/empty_json.txt
+python3 ../../src/main/python/accept.py tests/bad_test_type.test          > out/bad_test_type.txt
+python3 ../../src/main/python/accept.py tests/missing_shell_command.test  > out/missing_shell_command.txt
+python3 ../../src/main/python/accept.py tests/echo_just_command.test      > out/echo_just_command.txt
+python3 ../../src/main/python/accept.py tests/echo_exit_0.test            > out/echo_exit_0.txt
+python3 ../../src/main/python/accept.py tests/echo_exit_1.test            > out/echo_exit_1.txt
+python3 ../../src/main/python/accept.py tests/echo_dog.test               > out/echo_dog.txt
+
+diff correct/ out/
